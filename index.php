043441c6ee8622e28ccb837ff8d06186e4dc1801
cdeliens <cdeliens@gmail.com>
2013-04-03 11:34:10 -0600

    push

--- index.php
+++ index.php
@@ -7,16 +7,11 @@
   <div class="box">
     <?php get_template_part( 'loop', 'events' );?>
   </div>
-<!--   <div class="box">
+<!--     <div id="subtittle" class="box banner_bottom .style_1 .article">
       <a href="https://www.facebook.com/CervezaImperial/app_552369711440459" target="_blank" >
           <img src="http://www.25este75sur.com/banners/730X55.jpg" style="margin: 0px 0px 20px 100px;"/> 
       </a>
   </div> -->
-    <div id="subtittle" class="box banner_bottom .style_1 .article">
-      <a href="https://www.facebook.com/CervezaImperial/app_552369711440459" target="_blank" >
-          <img src="http://www.25este75sur.com/banners/730X55.jpg" style="margin: 0px 0px 20px 100px;"/> 
-      </a>
-  </div>
     <div id="subtittle" class="box subtitle galleries .style_1 .article">
       <a href="<?php echo bloginfo('url') ?>/category/galeria" tittle="Galerias">galerias...</a>
       <a class="right" href="<?php echo bloginfo('url') ?>/category/galeria" tittle="+ galerias">+ galerias...</a>
